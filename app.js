16ff66a3097b8f3cd2991533e6986774079e2a6d
Sean Wangelin <seanwangelin@outlook.com>
2025-11-03 21:00:41 -0600

    createDeck function

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 let deck = [];
+
+const createDeck = () => {
+    deck = [];
+    for (const value of cardValues) {
+        for (const suit of suits) {
+            let weight = parseInt(value);
+            if (value === 'J' || value === 'Q' || value === 'K') weight = 10;
+            if (value === 'A') weight = 11;
+
+            let card = { value: value, suit: suit, weight: weight };
+            deck.push(card);
+            console.log(`Here is the deck: ${card.value} of ${card.suit} with weight ${card.weight}`);
+        }
+    }
+}
+
+createDeck();
